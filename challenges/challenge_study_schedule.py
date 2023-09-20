bb91fc368273e5ba0076ee36b67c2c82aa52b6ff
jeferson-viana <jefersonv28@gmail.com>
2023-09-20 16:24:34 -0300

    feat: Requisito 1 concluído

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,18 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+    if type(target_time) == int and target_time >= 0:
+        for student in permanence_period:
+            is_valid = (type(student[0]) == int and
+                        type(student[1]) == int)
+            if is_valid:
+                if target_time in list(range(student[0], student[1] + 1)):
+                    count += 1
+            else:
+                return
+        return count
+    return
+
+
+if __name__ == "__main__":
+    print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5),
+                          (4, 5), (4, 5), (6, 7)], 1))
