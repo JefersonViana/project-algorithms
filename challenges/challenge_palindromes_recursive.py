3b358fcc96e0de74f373c3c79bf59aa65ff025fe
jeferson-viana <jefersonv28@gmail.com>
2023-09-20 17:26:49 -0300

    feat: Requisito 2 concluído

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,15 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if high_index == -1:
+        return False
+    if low_index == high_index or low_index > high_index:
+        return True
+    else:
+        if word[low_index] == word[high_index]:
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+        else:
+            return False
+
+
+if __name__ == "__main__":
+    word = "GG"
+    print(is_palindrome_recursive(word, 0, len(word) - 1))
