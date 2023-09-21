631333c38f909a19fff79a7d875936bc79b65931
jeferson-viana <jefersonv28@gmail.com>
2023-09-21 10:01:05 -0300

    feat: Requisito 2 concluído

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    message = "abcdef"
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(10, 2)
+
+    assert encrypt_message(message, 2) == "fedc_ba"
+    assert encrypt_message(message, 3) == "cba_fed"
+    assert encrypt_message("batatinha", 10) == "ahnitatab"
