c32f3251d8b75e90ef132af6763028811b54af3b
jeferson-viana <jefersonv28@gmail.com>
2023-09-21 21:40:44 -0300

    feat: Implementando uma nova funcionalidade so requisito 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,5 @@
+# ESSE CÓDIGO ATÉ A LINHA 19 ESTÁ PASSANDO 100%,
+# MAS VOU DEIXAR UMA OUTRA SUGESTÃO QUE NÃO SUBIU PARA O GITHUB
 def is_valid(nums, num):
     is_validations = (
         type(num) != int or
@@ -16,6 +18,30 @@ def find_duplicate(nums):
             return False
         if nums[index] == nums[index + 1]:
             return num
+
+
+# UM CÓDIGO ALTERNATIVO PARA O CASO ACIMA,
+# LEMBRANDO QUE ESSA PARTE NÃO SUBIU PARA O GIT HUB, CONFERE OS (LOGS).
+# def is_valid(nums, num):
+#     is_validations = (
+#         type(num) != int or
+#         len(nums) == 2 and nums[0] != nums[1] or
+#         num < 1
+#     )
+#     return is_validations
+
+
+# def find_duplicate(nums):
+#     if len(nums) < 2:
+#         return False
+#     list_numbers = set([])
+#     for number in nums:
+#         if is_valid(nums, number):
+#             return False
+#         if number in list_numbers:
+#             return number
+#         else:
+#             list_numbers.add(number)
 
 
 # print(find_duplicate([1, 3, 4, 2, 2]))
